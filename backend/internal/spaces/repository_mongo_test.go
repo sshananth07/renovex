c8ec72bd0832241765ef1148954423ad37aab9ff
sshananth07 <sshananth07@gmail.com>
2026-09-11 09:09:18 +0800

    Skip integration tests in setupMongoDB when running with -short flag

--- backend/internal/spaces/repository_mongo_test.go
+++ backend/internal/spaces/repository_mongo_test.go
@@ -24,6 +24,9 @@ func defaultSpaceListRequest(t *testing.T) pagination.Request {
 }
 
 func setupMongoDB(t *testing.T) *mongo.Client {
+	if testing.Short() {
+		t.Skip("integration test: requires Docker/testcontainers; run without -short")
+	}
 	t.Helper()
 	ctx := context.Background()
 
